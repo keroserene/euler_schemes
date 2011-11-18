887d3e6ec50fed714cd47c2c05db1afa762f2689
Serene Han <keroserene@gmail.com>
2011-11-18 02:50:24 -0500

    Euler #42

--- pe4x.scm
+++ pe4x.scm
@@ -1,6 +1,43 @@
 ; Project Euler problems, 40-49
 ; keroserene
 
+; ------------ Problem 42 ------------
+; "triangle words in words.txt"
+; All caps locked.
+ 	(define (word->int w)
+	       (apply + (map (lambda(c) (+ 1 (- (char->integer c) (char->integer #\A)))) 
+	       		(string->list w))))
+ 
+(define (p42)
+	(define (word->int w)
+	       (apply + (map (lambda(c) (+ 1 (- (char->integer c) (char->integer #\A)))) 
+	       		(string->list w))))
+
+	; triangular number: t(t+1)/2
+	; 2x = t^2 + t -> t^2 + t - 2x = 0
+	; x = (-1 +- sqrt(1^2 + 4*1*2x))/2
+	; True if integral.
+	(define (tw? w) 
+		(and (> (string-length w) 0)
+		(integer? (/ (- (sqrt (+ 1 (* 4 2 (word->int w)))) 1) 2))))
+
+	(define (scan file)
+		(define port (open-input-file file))
+		(define (readword)
+			(define input (read port))
+			(if (string? input) input
+				(symbol->string (unquote input))))
+		(define (count cw acc)
+			(if (eof-object? (peek-char port)) acc
+			(let ((c (read-char port)))
+			(if (or (eq? c #\") (eq? c #\,))
+				(count "" (+ acc (if (tw? cw) 1 0)))
+				(count (string-append cw (string c)) acc)))))
+		(count "" 0))
+	(scan "words.txt"))
+ 
+
+
 ; ------------ Problem 45 ------------
 ; "Next Triangular number that is also pentagonal and hexagonal after 40755 (h_143)"
 
